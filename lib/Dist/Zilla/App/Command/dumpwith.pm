94e43819dc57c3e3d045d05d1deec511b77f0692
Kent Fredric <kentfredric@gmail.com>
2013-11-12 01:51:55 +1300

    First POC

--- lib/Dist/Zilla/App/Command/dumpwith.pm
+++ lib/Dist/Zilla/App/Command/dumpwith.pm
@@ -3,11 +3,134 @@ use warnings;
 
 package Dist::Zilla::App::Command::dumpwith;
 
-# ABSTRACT:
+# ABSTRACT: Dump all plugins that 'do' a certain role
 
-use Moose;
+=head1 SYNOPSIS
 
-__PACKAGE__->meta->make_immutable;
-no Moose;
+  cd $PROJECT;
+  dzil dumpwith -VersionProvider
+
+  dzil dumpwith -FileGatherer --color-theme=basic::plain # plain text
+  dzil dumpwith -BeforeRelease --color-theme=basic::green # green text
+
+If you are using an HTML-enabled POD viewer, you should see a screenshot of this in action:
+
+( Everyone else can visit L<http://kentfredric.github.io/Dist-Zilla-App-Command-dumpphases/media/example_01.png> )
+
+=for html <center><img src="http://kentfredric.github.io/Dist-Zilla-App-Command-dumpphases/media/example_01.png" alt="Screenshot" width="721" height="1007"/></center>
+
+=cut
+
+=head1 DESCRIPTION
+
+Working out what Plugins will execute in which order during which phase can be a
+little confusing sometimes.
+
+This Command exists primarily to make developing Plugin Bundles and debugging
+dist.ini a bit easier, especially for newbies who may not fully understand
+Bundles yet.
+
+If you want to turn colors off, use L<< C<Term::ANSIcolor>'s environment variable|Term::ANSIColor >>
+C<ANSI_COLORS_DISABLED>. E.g.,
+
+C<ANSI_COLORS_DISABLED=1 dzil dumpphases>
+
+Alternatively, since 0.3.0 you can specify a color-free theme:
+
+    dzil dumpphases --color-theme=basic::plain
+
+=cut
+
+=begin MetaPOD::JSON v1.1.0
+
+{
+    "namespace":"Dist::Zilla::App::Command::dumpphases",
+    "inherits":"Dist::Zilla::App::Command",
+    "interface":"class"
+}
+
+=end MetaPOD::JSON
+
+=cut
+
+use Dist::Zilla::App -command;
+use Moose::Autobox;
+use Try::Tiny;
+use Scalar::Util qw( blessed );
+
+## no critic ( ProhibitAmbiguousNames)
+sub abstract { return 'Dump all plugins that "do" a specific role' }
+## use critic
+
+sub opt_spec {
+  return ( [ 'color-theme=s', 'color theme to use, ( eg: basic::blue )' ] );
+}
+
+sub _has_module {
+  my ( $self, $module ) = @_;
+  require Module::Runtime;
+  try { Module::Runtime::require_module($module) }
+  catch {
+    require Carp;
+    Carp::cluck( "The module $module seems invalid, did you type it right? Is it installed?" );
+    die $_;
+  };
+}
+
+sub _has_dz_role {
+  my ( $self, $role ) = @_;
+  require Module::Runtime;
+  my $module = Module::Runtime::compose_module_name( 'Dist::Zilla::Role', $role );
+  try {
+    Module::Runtime::require_module($module);
+  }
+  catch {
+    require Carp;
+    Carp::cluck( "The role -$role seems invalid, did you type it right? Is it installed?" );
+    die $_;
+  };
+}
+
+sub validate_args {
+  my ( $self, $opt, $args ) = @_;
+  for my $arg ( @{$args} ) {
+    if ( $arg =~ /\A-(.*)\z/ ) {
+      $self->_has_dz_role($1);
+    }
+    else {
+      $self->_has_module($arg);
+    }
+  }
+}
+
+sub _get_color_theme {
+  my ( $self, $opt, $default ) = @_;
+  return $default unless $opt->color_theme;
+  return $opt->color_theme;
+}
+
+sub _get_theme_instance {
+  my ( $self, $theme ) = @_;
+  require Module::Runtime;
+  my $theme_module = Module::Runtime::compose_module_name( 'Dist::Zilla::dumpphases::Theme', $theme );
+  Module::Runtime::require_module($theme_module);
+  return $theme_module->new();
+}
+
+sub execute {
+  my ( $self, $opt, $args ) = @_;
+  my $zilla = $self->zilla;
+
+  my $theme = $self->_get_theme_instance( $self->_get_color_theme( $opt, 'basic::blue' ) );
+
+  for my $arg ( @{$args} ) {
+    $theme->print_section_prelude( 'role: ', $arg );
+    for my $plugin ( @{ $zilla->plugins_with($arg) } ) {
+      $theme->print_star_assoc( $plugin->plugin_name, blessed($plugin) );
+    }
+  }
+
+  return 0;
+}
 
 1;
