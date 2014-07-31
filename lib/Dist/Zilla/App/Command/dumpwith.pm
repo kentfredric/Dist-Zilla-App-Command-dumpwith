ab051162d1d147ef0cd1b6cc7f494d19459763f5
Kent Fredric <kentfredric@gmail.com>
2014-07-31 21:54:39 +1200

    Build results of 38e4312 (on master)

--- lib/Dist/Zilla/App/Command/dumpwith.pm
+++ lib/Dist/Zilla/App/Command/dumpwith.pm
@@ -65,7 +65,7 @@ our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
 use Dist::Zilla::App -command;
 use Moose::Autobox;
-use Try::Tiny;
+use Try::Tiny qw( try catch );
 use Scalar::Util qw( blessed );
 
 ## no critic ( ProhibitAmbiguousNames)
