6c64dcac09600ad1b74203446132706019cabdf3
Kent Fredric <kentfredric@gmail.com>
2015-04-13 08:40:47 +1200

    Build results of 3582c18 (on master)

--- lib/Dist/Zilla/App/Command/dumpwith.pm
+++ lib/Dist/Zilla/App/Command/dumpwith.pm
@@ -23,6 +23,7 @@ our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
 
 use Dist::Zilla::App '-command';
+use Try::Tiny qw( try catch );
 
 ## no critic ( ProhibitAmbiguousNames)
 sub abstract { return 'Dump all plugins that "do" a specific role' }
