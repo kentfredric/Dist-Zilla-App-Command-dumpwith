5862f3fb33d283446587906e02dea3ee40bfda98
Kent Fredric <kentfredric@gmail.com>
2015-04-15 12:37:14 +1200

    Build results of 744b81f (on master)

--- lib/Dist/Zilla/App/Command/dumpwith.pm
+++ lib/Dist/Zilla/App/Command/dumpwith.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::App::Command::dumpwith;
 
-our $VERSION = '0.002012';
+our $VERSION = '0.002013';
 
 # ABSTRACT: Dump all plugins that 'do' a certain role
 
@@ -161,7 +161,7 @@ Dist::Zilla::App::Command::dumpwith - Dump all plugins that 'do' a certain role
 
 =head1 VERSION
 
-version 0.002012
+version 0.002013
 
 =head1 SYNOPSIS
 
